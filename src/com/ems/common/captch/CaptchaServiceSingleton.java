23af67e30bec70f0f8227f68eed2316edf5c45c5
FoxWhite <fmjj008@gmail.com>
2021-05-01 00:33:33 +0900

    이미지 갯차 삭제.

--- src/com/ems/common/captch/CaptchaServiceSingleton.java
+++ src/com/ems/common/captch/CaptchaServiceSingleton.java
@@ -1,53 +1,47 @@
 package com.ems.common.captch;
 
-import com.octo.captcha.engine.CaptchaEngine;
-import com.octo.captcha.service.captchastore.CaptchaStore;
-import com.octo.captcha.service.captchastore.FastHashMapCaptchaStore;
-import com.octo.captcha.service.image.DefaultManageableImageCaptchaService;
-import com.octo.captcha.service.image.ImageCaptchaService;
-
 public class CaptchaServiceSingleton {
 
-	   private static CaptchaServiceSingleton INSTANCE;
-	   private static Throwable thrownLoadingThisClass;
-	   private ImageCaptchaService imgcapa;
-
-
-	    static {
-	       // 모든 예외를 잡아내야 한다.
-	       try {
-	           INSTANCE = new CaptchaServiceSingleton();
-	       } catch (Throwable t) {
-	           thrownLoadingThisClass = t;
-	           t.printStackTrace(System.err);
-	       }
-	   }
-
-	   public static CaptchaServiceSingleton getInstance() {
-	       if (thrownLoadingThisClass == null) {
-	           return INSTANCE;
-	       } else {
-	           throw new IllegalStateException(
-	               "This class has not initialized properly. Check the root cause."
-	               , thrownLoadingThisClass);
-	       }
-	   }
-
-	   private CaptchaServiceSingleton() {
-	      // 초기화 작업...
-
-		   SimpleListImageCaptchaEngine engine = new SimpleListImageCaptchaEngine();
-
-		   imgcapa =  (ImageCaptchaService)new DefaultManageableImageCaptchaService((CaptchaStore)new FastHashMapCaptchaStore(), (CaptchaEngine)engine, 180, 100000, 75000);
-
-
-	   }
-	   // ... 나머지 코드
-
-
-	   public ImageCaptchaService getImgcapa() {
-	        return imgcapa;
-	    }
+//	   private static CaptchaServiceSingleton INSTANCE;
+//	   private static Throwable thrownLoadingThisClass;
+//	   private ImageCaptchaService imgcapa;
+//
+//
+//	    static {
+//	       // 모든 예외를 잡아내야 한다.
+//	       try {
+//	           INSTANCE = new CaptchaServiceSingleton();
+//	       } catch (Throwable t) {
+//	           thrownLoadingThisClass = t;
+//	           t.printStackTrace(System.err);
+//	       }
+//	   }
+//
+//	   public static CaptchaServiceSingleton getInstance() {
+//	       if (thrownLoadingThisClass == null) {
+//	           return INSTANCE;
+//	       } else {
+//	           throw new IllegalStateException(
+//	               "This class has not initialized properly. Check the root cause."
+//	               , thrownLoadingThisClass);
+//	       }
+//	   }
+//
+//	   private CaptchaServiceSingleton() {
+//	      // 초기화 작업...
+//
+//		   SimpleListImageCaptchaEngine engine = new SimpleListImageCaptchaEngine();
+//
+//		   imgcapa =  (ImageCaptchaService)new DefaultManageableImageCaptchaService((CaptchaStore)new FastHashMapCaptchaStore(), (CaptchaEngine)engine, 180, 100000, 75000);
+//
+//
+//	   }
+//	   // ... 나머지 코드
+//
+//
+//	   public ImageCaptchaService getImgcapa() {
+//	        return imgcapa;
+//	    }
 
 
 	}
--- src/com/ems/common/captch/ImageCaptchaServlet.java
+++ src/com/ems/common/captch/ImageCaptchaServlet.java
@@ -4,78 +4,61 @@
 
 package com.ems.common.captch;
 
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
-import javax.imageio.stream.ImageOutputStream;
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-import org.apache.log4j.Logger;
+public class ImageCaptchaServlet extends HttpServlet {
+//    private Logger log;
+//    private static final long serialVersionUID = 1L;
+//
+//    public ImageCaptchaServlet() {
+//        this.log = Logger.getLogger((Class)this.getClass());
+//    }
+//
+//    public void init(final ServletConfig servletConfig) throws ServletException {
+//        super.init(servletConfig);
+//    }
+//
+//    protected void doGet(final HttpServletRequest httpServletRequest, final HttpServletResponse httpServletResponse) throws ServletException, IOException {
+//        byte[] captchaChallengeAsJpeg = null;
+//        final ByteArrayOutputStream jpegOutputStream = new ByteArrayOutputStream();
+//
+//        try {
+//            final String captchaId = httpServletRequest.getSession().getId();
+//            final ImageCaptchaService imgcp = CaptchaServiceSingleton.getInstance().getImgcapa();
+//            final BufferedImage challenge = imgcp.getImageChallengeForID(captchaId, httpServletRequest.getLocale());
+////            final JPEGImageEncoder jpegEncoder = JPEGCodec.createJPEGEncoder((OutputStream)jpegOutputStream);
+////            jpegEncoder.encode(challenge);
+//
+//
+//            JPEGImageWriter imageWriter = (JPEGImageWriter) ImageIO.getImageWritersBySuffix("jpeg").next();
+//
+//            ImageOutputStream ios = ImageIO.createImageOutputStream(jpegOutputStream);
+//            imageWriter.setOutput(ios);
+//
+//
+//        }
+//        catch (IllegalArgumentException e) {
+//            httpServletResponse.sendError(404);
+//            return;
+//        }
+//        catch (CaptchaServiceException e2) {
+//            httpServletResponse.sendError(500);
+//            return;
+//        }catch(Exception e) {
+//        	e.printStackTrace();
+//        }
+//
+//
+//        captchaChallengeAsJpeg = jpegOutputStream.toByteArray();
+//        httpServletResponse.setHeader("Cache-Control", "no-store");
+//        httpServletResponse.setHeader("Pragma", "no-cache");
+//        httpServletResponse.setDateHeader("Expires", 0L);
+//        httpServletResponse.setContentType("image/jpeg");
+//        final ServletOutputStream responseOutputStream = httpServletResponse.getOutputStream();
+//        responseOutputStream.write(captchaChallengeAsJpeg);
+//        responseOutputStream.flush();
+//        responseOutputStream.close();
+//        System.out.println("responseOutputStream.close");
+//    }
 
-import com.octo.captcha.service.CaptchaServiceException;
-import com.octo.captcha.service.image.ImageCaptchaService;
-import com.sun.imageio.plugins.jpeg.JPEGImageWriter;
-
-public class ImageCaptchaServlet extends HttpServlet
-{
-    private Logger log;
-    private static final long serialVersionUID = 1L;
-
-    public ImageCaptchaServlet() {
-        this.log = Logger.getLogger((Class)this.getClass());
-    }
-
-    public void init(final ServletConfig servletConfig) throws ServletException {
-        super.init(servletConfig);
-    }
-
-    protected void doGet(final HttpServletRequest httpServletRequest, final HttpServletResponse httpServletResponse) throws ServletException, IOException {
-        byte[] captchaChallengeAsJpeg = null;
-        final ByteArrayOutputStream jpegOutputStream = new ByteArrayOutputStream();
-
-        try {
-            final String captchaId = httpServletRequest.getSession().getId();
-            final ImageCaptchaService imgcp = CaptchaServiceSingleton.getInstance().getImgcapa();
-            final BufferedImage challenge = imgcp.getImageChallengeForID(captchaId, httpServletRequest.getLocale());
-//            final JPEGImageEncoder jpegEncoder = JPEGCodec.createJPEGEncoder((OutputStream)jpegOutputStream);
-//            jpegEncoder.encode(challenge);
-
-
-            JPEGImageWriter imageWriter = (JPEGImageWriter) ImageIO.getImageWritersBySuffix("jpeg").next();
-
-            ImageOutputStream ios = ImageIO.createImageOutputStream(jpegOutputStream);
-            imageWriter.setOutput(ios);
-
-
-        }
-        catch (IllegalArgumentException e) {
-            httpServletResponse.sendError(404);
-            return;
-        }
-        catch (CaptchaServiceException e2) {
-            httpServletResponse.sendError(500);
-            return;
-        }catch(Exception e) {
-        	e.printStackTrace();
-        }
-
-
-        captchaChallengeAsJpeg = jpegOutputStream.toByteArray();
-        httpServletResponse.setHeader("Cache-Control", "no-store");
-        httpServletResponse.setHeader("Pragma", "no-cache");
-        httpServletResponse.setDateHeader("Expires", 0L);
-        httpServletResponse.setContentType("image/jpeg");
-        final ServletOutputStream responseOutputStream = httpServletResponse.getOutputStream();
-        responseOutputStream.write(captchaChallengeAsJpeg);
-        responseOutputStream.flush();
-        responseOutputStream.close();
-        System.out.println("responseOutputStream.close");
-    }
 }
